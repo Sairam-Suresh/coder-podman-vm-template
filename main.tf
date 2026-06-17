09b03845d6632890502fb0a784db52d1e2dda305
Sairam-Suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2026-06-17 09:46:24 +0800

    change slug to something valid

--- main.tf
+++ main.tf
@@ -567,7 +567,7 @@ module "code-server-subagent" {
   extensions = ["catppuccin.catppuccin-vsc-icons", "github.vscode-pull-request-github", "catppuccin.catppuccin-vsc"]
 
   open_in = "tab"
-  slug    = "Code Server (Devcontainer)"
+  slug    = "code-server-devcontainer"
 
   settings = {
     "git.autofetch": true,
